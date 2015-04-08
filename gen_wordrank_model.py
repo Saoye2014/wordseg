357e42321daaecee5ee2d0f277140b97630a54ef
shiyanshi <qq@gmail.com>
2015-04-08 21:19:26 +0800

    to be checked

--- gen_wordrank_model.py
+++ gen_wordrank_model.py
@@ -5,6 +5,7 @@ import sys
 import pprint
 import glob
 import math
+import time
 #@param ladj={}
 #@param radj={}
 #@param wordhyp={}
@@ -41,10 +42,6 @@ def cal_EBV(wordhyp, ladj, radj, iter_times):
         for word in wordhyp:
             wordhyp_bv[word]["lbv"]=wordhyp_bv[word]["lbv"]/math.sqrt(sum_lv)
             wordhyp_bv[word]["rbv"]=wordhyp_bv[word]["rbv"]/math.sqrt(sum_rv)
-
-        for word in wordhyp:
-            wordhyp_bv[word]["lbv"]=wordhyp_bv[word]["lbv"]/math.sqrt(sum_lv)
-            wordhyp_bv[word]["rbv"]=wordhyp_bv[word]["rbv"]/math.sqrt(sum_rv)
     
     for word in wordhyp:
         wordhyp_ebv[word]=wordhyp_bv[word]["lbv"]*wordhyp_bv[word]["rbv"]
@@ -59,14 +56,13 @@ def cal_IBV(wordhyp, prop_character, size):
 #    print raw_trainning_text
 #    print "ss",raw_xy[1],"ss"
     for word in wordhyp:
+        if len(word)==1: # 单字的内部边界值暂时令x=y
+            ibv = 1/prop_character[word]
+            wordhyps_ibv[word]=math.log(ibv*size,2)
+            continue 
         for i in range(len(word)-1):
             xy = word[i:i+2]
             if prop_xy.has_key(xy):
-                ibv = (prop_xy[xy]/(prop_character[xy[0]]*prop_character[xy[1]]))
-                if ibv ==0:
-                    ibv=1
-                else :
-                    ibv = math.log(ibv,2)
                 ibv = prop_xy[xy]/(prop_character[xy[0]]*prop_character[xy[1]])
             else:
                 ibv = prop_character[xy]/(prop_character[xy[0]]*prop_character[xy[1]])   #避免再次查询prop，提高效率？
@@ -85,18 +81,41 @@ def cal_IBV(wordhyp, prop_character, size):
 def cal_BV(wordhyp, ladj, radj, iter_times,prop_character,size):
     wordhyps_ebv = cal_EBV(wordhyp,ladj,radj,iter_times)
     wordhyps_ibv = cal_IBV(wordhyp,prop_character,size)
+#    i = 2
+    cur_time = time.strftime('%m-%d-%H-%M',time.localtime(time.time()))
+#    while i<7.2:
+#        wordhyps_wrv = {}#WordRank Value
+#        for word in wordhyp:
+#            print "ebv",
+#            print wordhyps_ebv[word]
+#            print "ibv",
+#            print wordhyps_ibv[word]
+#            wordhyps_wrv[word] = wordhyps_ebv[word] * math.pow(i,wordhyps_ibv[word]) #TUDO
+            
+#        dir = cur_time +'/'+str(i)+'exp'
+#        os.makedirs(dir)
+#        print "dump ebv,ibv,wr_model   ..."
+#        pprint.pprint(wordhyps_ebv,open(dir+"/ebv.txt","wb"))
+#        pprint.pprint(wordhyps_ibv,open(dir+"/ibv.txt","wb"))
+#        pprint.pprint(wordhyps_wrv,open(dir+"/wr_model.txt","wb"))
+#        print "done"
+#        i=i+0.3
     
-    wordhyp_wrv = {}#WordRank Value
-    for word in wordhyp:
-        print "ebv",
-        print wordhyps_ebv[word]
-        print "ibv",
-        print wordhyps_ibv[word]
-        wordhyp_wrv[word] = wordhyps_ebv[word] * math.pow(wordhyps_ibv[word],4.4) 
-    print "dump ebv,ibv,wr_model   ..."
-    pprint.pprint(wordhyps_ebv,open("ebv.txt","wb"))
-    pprint.pprint(wordhyps_ibv,open("ibv.txt","wb"))
-    pprint.pprint(wordhyps_wrmodel,open("wr_model.txt","wb"))
-    print "done"
-    return wordhyp_wrv
+    i=4.6
+    while i<5.5:
+        wordhyps_wrv = {}#WordRank Value
+        for word in wordhyp:
+            wordhyps_wrv[word] = wordhyps_ebv[word] * math.pow(abs(wordhyps_ibv[word]),i) #TUDO
+            
+        dir = cur_time +'/'+str(i)+'ploy'
+        os.makedirs(dir)
+        print "dump ebv,ibv,wr_model   ..."
+        pprint.pprint(wordhyps_ebv,open(dir+"/ebv.txt","wb"))
+        pprint.pprint(wordhyps_ibv,open(dir+"/ibv.txt","wb"))
+        pprint.pprint(wordhyps_wrv,open(dir+"/wr_model.txt","wb"))
+        print "done"
+        i=i+0.2
+
+    
+    return wordhyps_wrv
 #LOT NEED TODO
